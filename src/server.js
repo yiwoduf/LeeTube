e3a05883f7781c9c11eb4a9fb40de6143264c680
leeable <yiwoduf@gmail.com>
2024-02-11 16:17:13 -0600

    Morgan Middleware Init

--- src/server.js
+++ src/server.js
@@ -1,30 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method}: ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("you may enter");
-  next();
-};
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
   return res.send("home");
 };
 
+const login = (req, res) => {
+  return res.send("login");
+};
+
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
+
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
