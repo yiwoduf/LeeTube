0f1cf4b85ada446f856466ff66a971633e3c3592
leeable <yiwoduf@gmail.com>
2024-02-11 09:09:54 -0600

    Basic Server Function Setup
    - Blueprints for middlewares and controllers

--- src/server.js
+++ src/server.js
@@ -4,16 +4,27 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method}: ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("you may enter");
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("Hi Kawon!");
+  return res.send("home");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login Page");
-};
-
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
