a5bdcc3742b2beb0929a12cfa19f592ca755edee
leeable <yiwoduf@gmail.com>
2024-02-22 14:45:06 -0600

    Setup Mixins & Test DB
    - Created Videos Mixin
    - Created Test DB (JSON) to test Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,46 @@
-export const trending = (req, res) => res.render("home");
+const testUser = {
+  username: "Lee",
+  loggedIn: false,
+};
 
-export const view = (req, res) => res.render("watch");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 100,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 100,
+      id: 1,
+    },
+    {
+      title: "Video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 100,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", testUser, videos });
+};
 
-export const edit = (req, res) => res.render("edit");
+export const view = (req, res) =>
+  res.render("watch", { pageTitle: "Watch", videoID: `#${req.params.id}` });
 
-export const search = (req, res) => res.send("Search");
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
-export const upload = (req, res) => res.send("Upload");
+export const search = (req, res) => res.send("Search", { pageTitle: "Search" });
+
+export const upload = (req, res) => res.send("Upload", { pageTitle: "Upload" });
 
 export const deleteVideo = (req, res) => {
   console.log(req.params);
--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
+/* SETUP EXPRESS SERVER */
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
+/* SET NAVIGATORS & VIEW ENGINE */
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
