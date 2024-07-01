5406e6c8e15f85f4c71fb18bd645328f99ad2b9a
leeable <yiwoduf@gmail.com>
2024-07-01 14:03:57 -0500

    Hide Morgan Logger

--- src/server.js
+++ src/server.js
@@ -6,12 +6,13 @@ import videoRouter from "./routers/videoRouter";
 
 /* SETUP EXPRESS SERVER */
 const app = express();
+/* MORGAN HTTP REQ LOG (DEV) */
 const logger = morgan("dev");
 
 /* SET NAVIGATORS & VIEW ENGINE */
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-app.use(logger);
+//app.use(logger);
 app.use(express.urlencoded({ extended: true })); // HTML form data to JS
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
