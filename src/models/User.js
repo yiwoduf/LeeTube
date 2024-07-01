9fc06d0035c360631264714cb5761e8f2410ed1c
leeable <yiwoduf@gmail.com>
2024-07-01 16:25:07 -0500

    bcrypt for user PW hashing
    - bcrypt package used to hash user password
    - Mongoose pre function for hashing implemented

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  console.log("User password: ", this.password);
+  this.password = await bcrypt.hash(this.password, 3);
+  console.log("Hashed: ", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
